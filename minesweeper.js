f1f2cec51c5832d0d4db3dffb2131b67ce83c38f
Sungmiii <christyksmile@gmail.com>
2018-08-14 13:23:26 +1200

    3 row 3 col so far

--- minesweeper.js
+++ minesweeper.js
@@ -7,6 +7,11 @@ var board = {
       {row:2,col:2,isMine:true,hidden:true},
       {row:0,col:1,isMine:false,hidden:true},
       {row:2,col:1,isMine:true,hidden:true},
+      {row:1,col:2,isMine:false,hidden:true},
+      {row:3,col:2,isMine:true,hidden:true},
+      {row:2,col:3,isMine:true,hidden:true},
+      {row:3,col:1,isMine:false,hidden:true},
+      {row:1,col:0,isMine:true,hidden:true},
 
   ]
 }
